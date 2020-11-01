3fd8b0ffcef44760bda5da6c0473d32db176a445
Wiemann <marc.wiemann@tk-steel.com>
2020-11-01 18:00:27 +0100

    Firebase added

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'style.dart';
 import 'package:my_new_test_app/style.dart';
 import 'Screens/home_screen.dart';
@@ -6,13 +7,32 @@ import 'Screens/notiz_detail.dart';
 import "Screens/splashscreen.dart";
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: _routes(),
-      title: 'Notice',
-      theme: _theme(),
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return Text('Hier ist etwas schief gelaufen!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ));
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              onGenerateRoute: _routes(),
+              title: 'Notice',
+              theme: _theme(),
+            );
+          }
+          return CircularProgressIndicator();
+        });
   }
 }
 
